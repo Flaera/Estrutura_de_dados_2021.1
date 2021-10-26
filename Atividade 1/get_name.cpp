3324b3bd023aadf8da641c85af0dc3e5123ec932
Rafael <rafael2mcont@gmail.com>
2021-10-26 12:52:32 -0300

    get_name.cpp improvement with checkin of Uppercase into words

--- Atividade 1/get_name.cpp
+++ Atividade 1/get_name.cpp
@@ -16,9 +16,36 @@ char GetString(char string[]){
 void GetName(int len, char string[]){
     char initial = GetString(string);
     while ((initial < 65) || (initial > 90)){
-        printf("Nome deve começar apenas com letra Maiúscula.\n");
+        printf("O nome deve começar apenas com letra Maiúscula.\n");
         initial = GetString(string);
-        DEBUG{printf("char_initial2-%d-\n", initial);}
+    }
+    bool invalid_upper_case = false;
+    int acc = 0;
+    while (string[acc]!=0){
+        int acc2 = acc+1;
+        while ((string[acc2]!=32) || (string[acc2]!=0)){
+            if (((string[acc2]<97) || (string[acc2]>122)) && (string[acc2]!=32) && (string[acc2]!=0)){
+                DEBUG{printf("str:%d-\n", string[acc2]);}
+                invalid_upper_case = true;
+                break;
+            }
+            else if (string[acc2]==32){
+                acc2 += 1;
+                break;
+            }
+            else if (string[acc2]==0){
+                break;
+            }
+            acc2 += 1;
+        }
+        if (invalid_upper_case==true){
+            break;
+        }
+        acc = acc2;
+    }
+    if (invalid_upper_case==true){
+        printf("O nome não pode conter maiúsculas ou outros caracteres no meio de cada palavra.\n");
+        GetName(len, string);
     }
     return;
 }
