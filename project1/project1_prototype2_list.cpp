68c442b15358b4d5cd04e5e4b82ecf9671f10196
Rafael <rafael2mcont@gmail.com>
2021-11-14 16:18:32 -0300

    More implementations of list: remove method,  showall method and more

--- project1/project1_prototype2_list.cpp
+++ project1/project1_prototype2_list.cpp
@@ -1,4 +1,4 @@
-//#include <stdio.h>
+#include <stdio.h>
 #include <iostream>
 #define DEBUG if(1)
 #define LIST_FULL "List full.\n"
@@ -12,13 +12,13 @@ class List{
     private:
         int curr;
         int curr_size;
-        int *array;
         int max_size;
+        int *array;
     public:
-        List(int max_size){
+        List(int max_size2){
             curr = 0;
             curr_size = 0;
-            max_size = max_size;
+            max_size = max_size2;
             array = new int[max_size];
         }
         ~List(){
@@ -26,13 +26,19 @@ class List{
         }
         void Insert(int element){
             if ((curr_size!=0) && (curr_size<max_size)){
+                DEBUG{cout<<"AQUImax_size1: " << max_size << endl;}
                 for (int j=curr_size; j>curr; j--){
                     array[j] = array[j-1];
                 }
             }
-            //else if (curr_size<max_size){
-                //
-            //}
+            else if (curr_size>=max_size-1){
+                DEBUG{cout<<"AQUImax_size2: " << max_size << endl;}
+                cout << LIST_FULL;
+                array = new int[2*max_size]; //It can generated out of memory
+                for (int j=curr_size; j>curr; j--){
+                    array[j] = array[j-1];
+                }
+            }
             
             array[curr] = element;
             //curr+=1;
@@ -45,6 +51,7 @@ class List{
                     array[i] = array[i+1];
                 }
                 curr_size--;
+                cout << "Element in current position was removed\n";
             }
             else{
                 cout << LIST_EMPTY;
@@ -60,6 +67,7 @@ class List{
         int SetCurrent(int index){
             if (index<curr_size){
                 curr = index;
+                cout << "Setting for current element: " << array[curr] << endl;
                 return array[curr];
             }
             cout << "Index parameter most that current size list.\n";
@@ -75,23 +83,73 @@ class List{
         }
 };
 
+
+int ShowMenu(){
+    //1 for insert
+    //2 get current
+    //3 set  current
+    //4 for remove
+    //5 exit
+    cout << "1 - insert one element in current position.\n";
+    cout << "2 - get current element.\n";
+    cout << "3 - set current element.\n";
+    cout << "4 - remove element current.\n";
+    cout << "5 - show all elements.\n";
+    cout << "6 - exit.\n";
+    cout << "Digit one option:\n";
+    int opt;
+    cin >> opt;
+    getchar();
+    if (opt==1){return 1;}
+    else if (opt==2){return 2;}
+    else if (opt==3){return 3;}
+    else if (opt==4){return 4;}
+    else if (opt==5){return 5;}
+    else if (opt==6){return 6;}
+    else{return ShowMenu();}
+}
+
+
 int main(){
+    bool exit = false;
     int len;
+    cout << "Digit lenght of array:\n";
     cin >> len;
+    getchar();
     List list(len);
-
-    list.Insert(11);
-    list.Insert(456);
-    list.Insert(2);
-    list.Insert(4);
-    cout << "GetCurrent: " << list.GetCurrent() << endl;
-    cout << "SetCurrent: " << list.SetCurrent(1) << endl;
-    list.Remove();
-    list.Remove();
-    list.Remove();
-
-    list.ShowAll();
-
+    while (exit==false){
+        int opt = ShowMenu();
+        if (opt==6){return 0;}
+        else if (opt==1){
+            cout << "Digit one number to inserted:\n";
+            int element;
+            cin >> element;
+            getchar();
+            list.Insert(element);
+        }
+        else if (opt==2){
+            int element = list.GetCurrent();
+            if (element==-1){
+                cout << LIST_EMPTY;
+            }
+            else{
+                cout << "Current element: " << list.GetCurrent() << endl;
+            }
+        }
+        else if (opt==3){
+            cout << "Digit the index position to set:\n";
+            int index;
+            cin >> index;
+            getchar();
+            list.SetCurrent(index);
+        }
+        else if (opt==4){
+            list.Remove();
+        }
+        else if (opt==5){
+            list.ShowAll();
+        }
+    }
 
     return 0;
 }
