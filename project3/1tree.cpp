f71c28dc21c0a4d4793d476b3592857e87ef2974
João Victor Araujo <jvsa@ic.ufal.br>
2021-12-06 16:29:06 -0300

    Compile error removed
    Compile error removed, but insertAux() is wrong. Also removed C libraries headers

--- project3/1tree.cpp
+++ project3/1tree.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#define DEBUG if(0)
+#include <string>
+#define DEBUG if(1)
 
 using namespace std;
 
@@ -28,30 +29,38 @@ template <typename N> class CreateNode{
 };*/
 
 template <typename E> class Link {
+    private:
+        Link<E>* left;
+        Link<E>* right; // Pointer to next node in list
+        string name_city;
+        int positions[2];
     public:
-        E element; // Value for this node
-        Link *next; // Pointer to next node in list
         // Constructors
-        Link(const E& elemval, Link* nextval =NULL)
-            { element = elemval; next = nextval; }
-        Link(Link* nextval =NULL) { next = nextval; }
+        Link(string city, int posX, int posY){
+            this->name_city = city;
+            this->positions[0] = posX;
+            this->positions[1] = posY;
+            left = NULL;
+            right = NULL;
+            cout << "Node created." << endl;
+        }
+        string getNameCity(){return name_city;}
+        int getPosX(){return positions[0];}
+        int getPosY(){return positions[1];}
+        Link<E>* getLeft(){return left;}
+        Link<E>* getRight(){return right;}
+        void setLeftNode(Link<E>* left2){left = left2;}
+        void setRightNode(Link<E>* right2){right = right2;}
 };
 
 template <typename T> class nodeTree{
-    private:
-        nodeTree<T>* root;  
-        string name_city;   
-        int position[2];    //x and y coordinates of the city
-        nodeTree<T>* left;
-        nodeTree<T>* right;
+    //private:
+        //Link<T> *root;
     public:
-        nodeTree(string city, int posX, int posY){  //constructor with 3 parameters
-            name_city = city;
-            position[0] = posX;
-            position[1] = posY;
-            left = NULL;
-            right = NULL;
-            cout << "Node root created. Tree created." << endl;
+        Link<T>* root;
+        nodeTree(){  //constructor with 3 parameters
+            root = NULL;
+            cout << "Node root created." << endl;
         };
 
         void insert(){
@@ -59,47 +68,94 @@ template <typename T> class nodeTree{
             int posX, posY;
             cout << "Enter the name of the city: ";
             cin >> city;
+            getchar();
             cout << "Enter the x coordinate of the city: ";
             cin >> posX;
             cout << "Enter the y coordinate of the city: ";
             cin >> posY;
-            if (root->left == NULL){
-                root->left = new nodeTree<T>(city, posX, posY);
-                return;
+
+            if (root==NULL){
+                root = new Link<T>(city, posX, posY);
             }
-            else if (root->right==NULL){
-                root->right = new nodeTree<T>(city, posX, posY);
-                return;
+            else{
+                insertAux(root, city, posX, posY);
             }
-            //insert(root->left); // to left
-            //insert(root->right); // to right
-            return;
-        };
+        }
+
+
+        void insertAux(Link<T>* root, string name_city, int posx, int posy){
+            if (root->getLeft()==NULL){
+                Link<T>* new_node = new Link<T>(name_city, posx, posy);
+                root->setLeftNode(new_node);
+            }
+            else{
+                insertAux(root->getLeft(), name_city, posx, posy);
+            }
+            if (root->getRight()==NULL){
+                Link<T>* new_node = new Link<T>(name_city, posx, posy);
+                root->setRightNode(new_node);
+            }
+            else{
+                insertAux(root->getRight(), name_city, posx, posy);
+            }
+        }
+
+
+        Link<T>* getRoot(){
+            return root;
+        }
+
+
+        void ShowAllInOrder(nodeTree<T> object){
+            //Link<T> root = object.getRoot();
+            if (object.root!=NULL){
+                ShowAllInOrder(object.root->getLeft(), 0);
+                cout << "-" << object.root->getNameCity() << "-" << endl;
+                ShowAllInOrder(object.root->getRight(), 0);
+            }
+        }
+    
+        void ShowAllInOrder(Link<T>* root, int a){
+            //Link<T> root = object.getRoot();
+            if (root!=NULL){
+                ShowAllInOrder(root->getLeft(), 0);
+                cout << "-" << root->getNameCity() << "-" << endl;
+                ShowAllInOrder(root->getRight(), 0);
+            }
+        }
 };
 
 
 void ShowMenu(){
-    cout << "Insert city." << endl;
-    cout << "Remove city by name." << endl;
-    cout << "Remove city by coordinate." << endl;
-    cout << "Search city by name." << endl;
-    cout << "Search city by coordinate." << endl;
-    cout << "Search by distance although of one point (x,y)." << endl;
+    cout << "1 - Insert city." << endl;
+    cout << "2 - Remove city by name." << endl;
+    cout << "3 - Remove city by coordinate." << endl;
+    cout << "4 - Search city by name." << endl;
+    cout << "5 - Search city by coordinate." << endl;
+    cout << "6 - Search by distance although of one point (x,y)." << endl;
+    cout << "7 - Show all cities." << endl;
+    cout << "8 - Show root of the tree." << endl;
+    cout << "9 - Exit the program." << endl;
     return;
 }
 
 
 int main(){
 
-    nodeTree<int> arvore("Rio", 2, 3);
+    nodeTree<int> arvore;
 
     int opt = 0;
-    while (opt!=9){
+    while (opt!=8){
         ShowMenu();
         cout << "Digit one option in list:" << endl;
         cin >> opt;
+        getchar();
         if (opt==1){
             arvore.insert();
+        }if (opt==7){
+            arvore.ShowAllInOrder(arvore);
+        }else if (opt==8){
+           cout << arvore.getRoot()<< endl << endl;
         }
     }
 
