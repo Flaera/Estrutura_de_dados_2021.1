c3f38c36ed033e7dea11749b9e4aee88dc49a66e
Rafael <rafael2mcont@gmail.com>
2021-12-10 16:33:15 -0300

    Quick sort implementation in classroom extra and radix sort implemented with bitshifting

--- aula_extra_10-12-2021/quick_sort.c
+++ aula_extra_10-12-2021/quick_sort.c
@@ -1,15 +1,55 @@
-Senão Se (n = 1)
-Então imprime v[0]
-Senão { pivo = v[0]
-para (i=1; i<10;i++){
-se (v[i] <= pivo)
-Então j = j + 1;
-x[j]= v[i];
-Senão z = z + 1;
-y[z]= v[i];
+#include <stdio.h>
+
+
+int Partition(int array[], int init, int end){
+    int left, right, aux, pivot;
+    left = init;
+    right = end;
+    pivot = array[init];
+    while (left<right){
+        while (array[left]<=pivot){left++;}
+        while (array[right]>pivot){right--;}
+        if (left<right){
+            aux = array[left];
+            array[left] = array[right];
+            array[right] = aux;
+        }
+    }
+    array[init] = array[right];
+    array[right] = pivot;
+    return right;
 }
-quick_sort(j+1,x);
-quick_sort(z+1,y);
+
+
+void QuickSort(int array[], int init, int end){
+    int pivot;
+    if (end > init){
+        pivot = Partition(array, init, end);
+        QuickSort(array, init, pivot-1);
+        QuickSort(array, pivot+1, end);
+    }
 }
-Return false
+
+
+int main(){
+    int size;
+
+    printf("Digit the lenght of vector: ");
+    scanf("%d", &size);
+    getchar();
+    int array[size];
+
+    printf("Digit the numbers of array: ");
+    for (int i=0; i<size; ++i){
+        scanf("%d", &array[i]);
+        getchar();
+    }
+    
+    QuickSort(array, 0, size);
+
+    printf("After quick sorting:\n");
+    for (int j=0; j<size; ++j){printf("%d ", array[j]);}
+    printf("\n\n");
+
+    return 0;
 }
