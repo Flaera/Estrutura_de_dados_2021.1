8c98370ee7103fbd5f0c3410ebad17d3e3891614
João Victor Araujo <86853316+jVictorSA@users.noreply.github.com>
2021-12-11 14:13:30 -0300

    Final adjustments

--- project4/radix_sort.cpp
+++ project4/radix_sort.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <stdio.h>
-#include <time.h>
+#include <chrono>
 #define DEBUG if(1)
 
 using namespace std;
+using namespace std::chrono;
 
 
 int getMax(int array[], int size){
@@ -70,14 +71,15 @@ int main(){
     cout << "Before sorting:" << endl;
     for (int k=0; k<size; ++k){cout << array[k] <<" ";}
     cout << endl;
-
-    time_t time_diff, t_end, t_init;
-    t_init = time(NULL);
+    auto start = high_resolution_clock::now();
+    
     RadixSort(array, size);
-    t_end = time(NULL);
-    time_diff = difftime(t_end, t_init);
-    printf("Radix sorting have delta time equal: %f.\n", (double)time_diff);
+    
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<nanoseconds>(stop - start);
 
+    cout << "Radix sorting have delta time equal: " << (double)duration.count() << " nanosseconds" << endl;
+    
     cout << "After sorting:" << endl;
     for (int l=0; l<size; ++l){cout << array[l] <<" ";}
     cout << endl;
