69f5c2062652791a1363a292c67e35087c04bb5b
Rafael <rafael2mcont@gmail.com>
2022-01-31 09:17:45 -0300

    Improvement class tree 2-3 and improvement method public in classes

--- project5/2-3tree.cpp
+++ project5/2-3tree.cpp
@@ -8,23 +8,30 @@ class Node{
         Node* left;
         Node* right;
         string word;
-
-    Node(string theWord){
-        this -> word = theWord;
-        this -> left = NULL;
-        this -> right = NULL;
-    }
-    Node(){}
-    ~Node(){}
+    public:
+        Node(string theWord){
+            this -> word = theWord;
+            this -> left = NULL;
+            this -> right = NULL;
+        }
+        Node(){}
+        ~Node(){}
 };
 
 class Tree_2_3{
     private:
         Node* root;
-
-
+    public:
+        Tree_2_3(){
+            cout << "Digit word of the node root: "
+            string word;
+            cin << word;
+        }
 };
 
 int main(){
     cout << "Ola";
+    
+    
+    return 0;
 }
