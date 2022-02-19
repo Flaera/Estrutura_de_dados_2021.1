cc9b8e64bd0fe3a430e37ab874e60dcbcec5f7ea
Rafael <rafael2mcont@gmail.com>
2022-02-19 15:50:03 -0300

    Implementation of djsktra

--- project6/graph.cpp
+++ project6/graph.cpp
@@ -228,6 +228,77 @@ class GraphL : public Graph{
         void setMark(int v, int val) {mark[v] = val;}
 };
 
+
+//Class graph with list adjacency and algorithm off Dijsktra:
+class GraphLDijsktra: public GraphL{
+    public:
+        int minVertex(Graph* G, int* D) { // Find min cost vertex
+            int i, v = -1;
+            // Initialize v to some unvisited vertex
+            for (i=0; i<G->n(); i++)
+                if (G->getMark(i) == UNVISITED) { v = i; break; }
+            for (i++; i<G->n(); i++) // Now find smallest D value
+                if ((G->getMark(i) == UNVISITED) && (D[i] < D[v]))
+                    v = i;
+            return v;
+        }
+
+        // Compute shortest path distances from "s".
+        // Return these distances in "D".
+        void Dijkstra(Graph* G, int* D, int s) {
+            int i, v, w;
+            for (int i=0; i<G->n(); i++)
+                // Initialize
+                D[i] = INFINITY;
+            D[0] = 0;
+            for (i=0; i<G->n(); i++) {
+                // Process the vertices
+                v = minVertex(G, D);
+                if (D[v] == INFINITY) return; // Unreachable vertices
+                G->setMark(v, VISITED);
+                for (w=G->first(v); w<G->n(); w = G->next(v,w))
+                    if (D[w] > (D[v] + G->weight(v, w)))
+                    D[w] = D[v] + G->weight(v, w);
+            }
+        }
+};
+
+
+//class graph with implementation of matrix adjacency of Dijsktra:
+class GraphLDijsktra: public GraphM{
+    public:
+        int minVertex(Graph* G, int* D) { // Find min cost vertex
+            int i, v = -1;
+            // Initialize v to some unvisited vertex
+            for (i=0; i<G->n(); i++)
+                if (G->getMark(i) == UNVISITED) { v = i; break; }
+            for (i++; i<G->n(); i++) // Now find smallest D value
+                if ((G->getMark(i) == UNVISITED) && (D[i] < D[v]))
+                    v = i;
+            return v;
+        }
+
+        // Compute shortest path distances from "s".
+        // Return these distances in "D".
+        void Dijkstra(Graph* G, int* D, int s) {
+            int i, v, w;
+            for (int i=0; i<G->n(); i++)
+                // Initialize
+                D[i] = INFINITY;
+            D[0] = 0;
+            for (i=0; i<G->n(); i++) {
+                // Process the vertices
+                v = minVertex(G, D);
+                if (D[v] == INFINITY) return; // Unreachable vertices
+                G->setMark(v, VISITED);
+                for (w=G->first(v); w<G->n(); w = G->next(v,w))
+                    if (D[w] > (D[v] + G->weight(v, w)))
+                    D[w] = D[v] + G->weight(v, w);
+            }
+        }
+};
+
+
 int main(){
 
 
