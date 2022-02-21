9966fab479f51fd82621ef4b54cadabac239f03c
Rafael <rafael2mcont@gmail.com>
2022-02-21 13:24:01 -0300

    Trying implementation menu and compiling

--- project6/graph.cpp
+++ project6/graph.cpp
@@ -4,6 +4,16 @@
     3 - usar grafos esparsos e densos
 */  
 #include <iostream>
+#define VISITED true
+#define UNVISITED false
+#define INFINITY 1  //aqui n sei qual é o valor desta diretriz, mas coloquei 1
+                    //porque em outro momento do código é dita como 0
+#define LIST_EMPTY "List empty."
+#define LIST_FULL "List fully."
+#define DEBUG if(0)
+
+using namespace std;
+
 
 //Classe abstrata de um grafo
 class Graph{
@@ -37,18 +47,242 @@ class Edge{
 //Classe de lista ligada para o grafo por lista de adjacência
 template <typename E>
 class List{
-    E List(){
+    // E List(){
+        
+    // };
+    private:
+        int curr, curr_size, max_size, initMaxSize;
+        int *array;
+    public:
+        List(int max_size2){
+            curr = 0;
+            curr_size = 0;
+            max_size = initMaxSize = max_size2;
+            array = new int[max_size];
+        }
+        ~List(){
+            delete array;
+        }
+        void Insert(int element){
+            if ((curr_size!=0) && (curr_size<max_size)){
+                DEBUG{cout<<"AQUImax_size1: " << max_size << endl;}
+                for (int j=curr_size; j>=curr; j--){
+                    array[j] = array[j-1];
+                }
+            }
+            else if (curr_size>=max_size-1){
+                doubleList(element);
+                return;
+            }
+            
+            array[curr] = element;
+            //curr+=1;
+            curr_size+=1;        
+            return;
+        }
 
-    };
+        void doubleList(int element){
+            DEBUG{cout<<"AQUImax_size2: " << max_size << endl;}
+            cout << LIST_FULL;
+            //Copying array:
+            int array_copy[max_size + 1];
+            for (int i=0; i<curr; ++i){
+               array_copy[i] = array[i];
+            }
+            array_copy[curr] = element;
+            curr_size+=1;
+            for (int j=curr+1; j<curr_size; ++j){
+                array_copy[j] = array[j-1];
+            }
+            DEBUG{
+                for (int l=0; l<curr_size; ++l){
+                    cout << "-" << array_copy[l] << "-" << endl;
+                }
+            }
+            //Creating new array
+            delete[] array;
+            max_size = 2*max_size;
+            array = new int[max_size]; //It can generated out of memory
+            for (int k=0; k<curr_size; ++k){
+                array[k] = array_copy[k];
+            }
+            cout << "Array doubled." << endl << endl;
+            return;
+        }
 
+        void Remove(){
+            if (curr_size!=0){
+                for (int i=curr; i<curr_size; ++i){
+                    array[i] = array[i+1];
+                }
+                curr_size--;
+                cout << "Element in current position was removed.\n";
+            }
+            else{
+                cout << LIST_EMPTY;
+                array[curr] = -1;
+            }
+        }
+
+        //Return element on the actual position on the list
+        int GetCurrentElem(){
+            if(curr_size == 0)
+                return -1;
+            
+            return array[curr];
+            //DEBUG{printf("curr:%d-\n", curr);}
+        }
+
+        int SetCurrent(int index){
+            if ((index<curr_size) && (index >= 0)){
+                curr = index;
+                cout << "Setting for current element: " << array[curr] << endl;
+                return array[curr];
+            }
+            cout << "Index parameter most that current size list." << endl;
+            return -1;
+        }
+
+        int length(){
+            return curr_size;
+        }
+
+        int maxSize(){
+            return max_size;
+        }
+
+        //Return actual position in the list
+        int getCurrentPos(){
+            return curr;
+        }
+
+        void nextPos(){
+            if(curr == curr_size)
+                cout << "No more positions ahead! Already at the end of the list." << endl << endl;
+            else{
+                curr += 1;
+                cout << "Moved 1 position to the rigth." << endl << endl;
+            }
+        }
+
+        void prevPos(){
+            if(curr == 0)
+                cout << "No more positions behind! Already at the beginning of the list." << endl << endl;
+            else{
+                curr -= 1;
+                cout << "Moved 1 position to the left." << endl << endl;
+            }
+        }
+
+        void moveToStart(){
+            if (curr == 1)
+                cout << "Already at the beginning of the list." << endl << endl;
+            else{
+                curr = 0;
+                cout << "Moved to the first position on the list." << endl << endl;
+            }
+        }
+
+        void moveToEnd(){
+            if (curr == curr_size)
+                cout << "Already at the end of the list." << endl << endl;
+            else{
+                curr = curr_size;
+                cout << "Moved to the last occupied position on the list." << endl << endl;
+            }
+        }
+
+        void clear(){
+            delete array;
+            curr = 0;
+            curr_size = 0;
+            max_size = initMaxSize;
+            array = new int[initMaxSize];
+        }
+
+        int find(int value){
+            for(int i = 0; i < curr_size; i++){
+                if(value == array[i]){
+                    cout << "The element " << value << " is in position " << i << "." << endl << endl;
+                    return 0;
+                }
+            }
+            
+            cout << "The element " << value << " isn't in the list." << endl << endl;
+
+            return 0;
+        }
+
+        void doubleListAppend(int element){
+            DEBUG{cout<<"AQUImax_size2: " << max_size << endl;}
+            cout << LIST_FULL;
+            //Copying array:
+            int array_copy[max_size + 1];
+            for (int i=0; i<curr_size; ++i){
+               array_copy[i] = array[i];
+            }
+            curr_size+=1;
+            array_copy[curr_size-1] = element;
+            DEBUG{
+                for (int l=0; l<curr_size; ++l){
+                    cout << "-" << array_copy[l] << "-" << endl;
+                }
+            }
+            //Creating new array
+            delete[] array;
+            max_size = 2*max_size;
+            array = new int[max_size]; //It can generated out of memory
+            for (int k=0; k<curr_size; ++k){
+                array[k] = array_copy[k];
+            }
+            cout << "Array doubled." << endl << endl;
+            return;
+        }
+
+        void append(int value){
+            if(curr_size == max_size){
+                doubleListAppend(value);
+            }else{
+                array[curr_size] = value;
+                curr_size += 1;
+            }
+        }
+
+        void ShowAll(){
+            cout << endl << "#####" << endl;
+            for (int i=0; i<curr_size; ++i){
+                cout << '-' << array[i] << '-' << endl;
+            }
+            cout << "#####" << endl << endl ;
+        }
 };
 
 template <typename E>
 class LList: public List{
-    E LList(){
+    // E LList(){
 
-    };
-
+    // };
+    private:
+        bool status_list;
+        int *array;
+        int len2;
+    public:
+        LList(int len){
+            len2 = len;
+            array = (int *)malloc(sizeof(List(len2)));
+            if (array==NULL){
+                cout << "Memory full. No space in the memory." << endl;
+                status_list = false;
+            }
+            cout << "Array created." << endl;
+            status_list = true;
+        }
+        ~LList(){
+            free(array);
+        }
+        bool Status(){
+            return status_list;
+        }
 };
 
 //Classe para o grafo utilizando matriz de adjacência
@@ -76,7 +310,7 @@ class GraphM : public Graph{
             mark = new int[n];
 
             for(i = 0; i < numVertex; i++){
-                //mark[i] = UNVISITED; //Tem escrito assim no livro mas não compila
+                mark[i] = UNVISITED; //Tem escrito assim no livro mas não compila
             }
             
             matrix = (int**) new int*[numVertex];
@@ -147,7 +381,7 @@ class GraphL : public Graph{
             numEdge = 0;
             mark = new int[n];
             for(i = 0; i < numVertex; i++){                
-                //mark[i] = UNVISITED ////Tem escrito assim no livro mas não compila
+                mark[i] = UNVISITED ////Tem escrito assim no livro mas não compila
             }
             vertex = (List<Edge>**) new List<Edge>*[numVertex];
             for(i = 0; i < numVertex; i++)
@@ -158,7 +392,7 @@ class GraphL : public Graph{
         int e() {return numEdge;}
 
         int first(int v){
-            if(vertex[v] -> lenght() == 0){
+            if(vertex[v] -> length() == 0){
                 return numVertex;
             }
             vertex[v] -> moveToStart();
@@ -187,7 +421,7 @@ class GraphL : public Graph{
             else{
                 numEdge++;
                 for(vertex[i] -> moveToStart();
-                    vertex[i] -> currPos() < vertex[i] -> lenght();
+                    vertex[i] -> currPos() < vertex[i] -> length();
                     vertex[i] -> next()){
                         Edge temp = vertex[i] -> getValue();
                         if(temp.vertex() > j) break;
@@ -206,7 +440,7 @@ class GraphL : public Graph{
         bool isEdge(int i, int j){
             Edge it;
             for(vertex[i] -> moveToStart();
-                vertex[i] -> currPos() < vertex[i] -> lenght();
+                vertex[i] -> currPos() < vertex[i] -> length();
                 vertex[i] -> next()){
                     Edge temp = vertex[i] -> getValue();
                     if(temp.vertex() == j)
@@ -303,14 +537,18 @@ int main(){
 
     int opt = 1;
     while (opt!=0){
-        cout<<"1 - Test graph with matrix of adjacency.\n";
-        cout<<"2 - Test graph with list of adjacency.\n";
-        cout<<"3 - Test graph with matrix of adjacency and algorithm of Dijsktra.\n";
-        cout<<"4 - Test graph with list of adjacency and algorithm of Dijsktra.\n";
-        cout<<"5 - Test with graph sparse.";
-        cout<<"6 - Test with graph dense.";
+        cout<<"1 - Test graph with matrix of adjacency."<<endl;
+        cout<<"2 - Test graph with list of adjacency."<<endl;
+        cout<<"3 - Test graph with matrix of adjacency and algorithm of Dijsktra."<<endl;
+        cout<<"4 - Test graph with list of adjacency and algorithm of Dijsktra."<<endl;
+        cout<<"5 - Test with graph sparse."<<endl;
+        cout<<"6 - Test with graph dense."<<endl;
+        cout<<"0 - Exit."<<endl;
         cout << "Digit one option: ";
         cin >> opt;
+        if (opt==1){
+            return 1;
+        }
     }
 
 
